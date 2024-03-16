327f74add966a7384c48179be830deb8d1da261e
AlbertoSac <99304548+AlbertoSac@users.noreply.github.com>
2024-03-15 18:29:47 -0600

    First one

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import coquetaImage from '../assets/coqueta.jpeg'; // Importa la imagen aquí
+import coquetaImage from '../assets/coqueta.jpeg';
 
 const Home = () => {
   return (
     <div className="hero-banner">
-      <div className="container text-center"> {/* Centramos el contenido */}
+      <div className="container text-center">
         <h1>¡Bienvenido a nuestra tienda de mascotas!</h1>
         <p>Descubre nuestra increíble selección de productos para tus mascotas.</p>
         <Link to="/products" className="btn btn-primary">Ver productos</Link>
       </div>
-      <div className="banner row justify-content-center"> {/* Centramos el banner */}
-        <div className="col-md-6 align-self-center text-center"> {/* Centramos verticalmente y horizontalmente el contenido */}
+      <div className="banner row justify-content-center">
+        <div className="col-md-6 align-self-center text-center"> 
           <img src={coquetaImage} alt="Producto destacado" style={{ maxWidth: '500px' }} />
           <div className="banner-content">
             <h3>Producto destacado</h3>
