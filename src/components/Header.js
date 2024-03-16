e70965f1b6ecff5f36dfcf109fc0fc2681d4d1a2
AlbertoSac <99304548+AlbertoSac@users.noreply.github.com>
2024-03-15 18:29:15 -0600

    First one

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ import logo from '../assets/pet-store.png';
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      {/* Margen a la izquierda del logo */}
       <Link className="navbar-brand ms-3" to="/"><img src={logo} alt="Pet Store" /></Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -24,7 +23,6 @@ const Header = () => {
         </ul>
         <form className="form-inline ms-auto mr-5">
         </form>
-        {/* Margen a la derecha de los elementos del lado derecho */}
         <ul className="navbar-nav ms-auto me-5">
           <li className="nav-item">
             <input className="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search" />
